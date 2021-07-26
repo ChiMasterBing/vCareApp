7481d20a0d190f47f5ad156f84bc4a076fe41aac
brandonzhu09 <brandonzhu123@gmail.com>
2021-07-26 09:44:11 -0400

    Update SavedScreen.java

--- app/src/main/java/com/example/vcare/SavedScreen.java
+++ app/src/main/java/com/example/vcare/SavedScreen.java
@@ -53,7 +53,8 @@ public class SavedScreen extends AppCompatActivity implements View.OnClickListen
                 System.out.println("Going to Articles page.");
                 break;
             case R.id.quotesButton:
-                System.out.println("Going to Quotes page.");
+                startActivity(new Intent(SavedScreen.this, DailyQuote.class));
+                finish();
                 break;
             case R.id.allSavedButton:
                 System.out.println("Going to All Saved page.");
